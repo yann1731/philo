f9d1e4ae7295671eda07abb5fccc78cad6321a6f
fenix144 <yan_1731@hotmail.com>
2022-06-05 21:36:08 -0400

    let's go

--- src/philo_act.c
+++ src/philo_act.c
@@ -1,5 +1,26 @@
 #include "../include/philosopher.h"
 
+// ◦timestamp_in_ms X has taken a fork
+// ◦timestamp_in_ms X is eating
+// ◦timestamp_in_ms X is sleeping
+// ◦timestamp_in_ms X is thinking
+// ◦timestamp_in_ms X died
+
+void	eat(t_philo *philo)
+{
+	printf("Philo")
+}
+
+void	think(t_philo *philo)
+{
+
+}
+
+void	sleep(t_philo *philo)
+{
+
+}
+
 void	*philo_act(void *arg)
 {
 	t_philo *philo;
