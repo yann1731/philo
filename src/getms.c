d91461ff513308cbd6505ad97cbc3b01065aad02
fenix144 <yan_1731@hotmail.com>
2022-06-20 10:06:04 -0400

    Removed comments

--- src/getms.c
+++ src/getms.c
@@ -7,12 +7,8 @@ int	getms(t_philo *philo)
 	int ms;
 
 	gettimeofday(&philo->current_time, NULL);
-	//printf("current time sec: %ld\n", philo->current_time.tv_sec);
-	//printf("start time sec: %ld\n", philo->start_time.tv_sec);
 	second = philo->current_time.tv_sec - philo->start_time.tv_sec;
-	//printf("second: %d\n", second);
 	microsecond = philo->current_time.tv_usec - philo->start_time.tv_usec;
-	//printf("microsecond: %d\n", microsecond);
 	ms = second * 1000;
 	ms += microsecond / 1000;
 	return (ms);
