6be48cf1e6fe6eb6541b10ff740139b4f337b78d
fenix144 <yan_1731@hotmail.com>
2022-06-21 16:50:06 -0400

    let's go

--- src/getms.c
+++ src/getms.c
@@ -7,8 +7,8 @@ int	getms(t_philo *philo)
 	int ms;
 
 	gettimeofday(&philo->current_time, NULL);
-	second = philo->current_time.tv_sec - philo->start_time.tv_sec;
-	microsecond = philo->current_time.tv_usec - philo->start_time.tv_usec;
+	second = philo->current_time.tv_sec - philo->args->start_time.tv_sec;
+	microsecond = philo->current_time.tv_usec - philo->args->start_time.tv_usec;
 	ms = second * 1000;
 	ms += microsecond / 1000;
 	return (ms);
