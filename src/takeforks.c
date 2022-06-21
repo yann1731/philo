80b55feb9dda99e05d15e5feb4c13505abc9097f
fenix144 <yan_1731@hotmail.com>
2022-06-21 18:43:22 -0400

    Add function to handle the philo grabbing his forks

--- src/takeforks.c
+++ src/takeforks.c
@@ -1,8 +1,19 @@
 #include "../include/philosopher.h"
 
-void	takeleftfork(t_philo *philo)
+void	takeforks(t_philo *philo)
 {
-	pthread_mutex_lock(&philo->pick_l_fork);
-	
-	pthread_mutex_unlock(&philo->pick_l_fork);
+	if (philo->args->forks[philo->id - 1] == 0)
+	{
+		philo->args->forks[philo->id - 1] = 1;
+		printf("%d %d has taken a fork\n", getms(philo), philo->id);
+	}
+	else
+		printf("%d %d could not take a fork\n", getms(philo), philo->id);
+	if (philo->args->forks[philo->id] == 0)
+	{
+		philo->args->forks[philo->id] = 1;
+		printf("%d %d has taken a fork\n", getms(philo), philo->id);
+	}
+	else
+		printf("%d %d could not take a fork\n", getms(philo), philo->id);
 }
