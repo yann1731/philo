cedd5de42459d00b726364079c6a08648fb2a25b
fenix144 <yan_1731@hotmail.com>
2022-06-21 16:50:53 -0400

    remove timeval struct to call gettimeofday directly on the timeval struct of args

--- src/initargs.c
+++ src/initargs.c
@@ -2,10 +2,15 @@
 
 void	initargs(char *argv[], t_args *args)
 {
+	int	i;
+
+	i = -1;
 	args->n_philo = ft_atoi(argv[1]);
 	args->philo = malloc(args->n_philo * sizeof(t_philo));
 	args->philo_thread = malloc(args->n_philo * sizeof(pthread_t));
 	args->forks = malloc(args->n_philo * sizeof(int));
+	while (++i < args->n_philo);
+		args->forks[i] = 0;
 	args->is_dead = FALSE;
 	initphilo(argv, args);
 }
