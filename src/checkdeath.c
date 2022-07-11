cae9204bab22d58c8644523ff8592462e0a44784
fenix144 <yan_1731@hotmail.com>
2022-07-10 20:23:10 -0400

    Add function to check for philo death

--- src/checkdeath.c
+++ src/checkdeath.c
@@ -2,6 +2,12 @@
 
 void	checkdeath(t_philo *philo)
 {
-	
-	philo->args->is_dead;
+	pthread_mutex_lock(&philo->check_dead);
+	printf("time since last meal: %d\n", getms(philo));
+	if ((getms(philo) - philo->t_s_last_meal) > philo->t_to_die)
+	{
+		philo->args->is_dead = TRUE;
+		printf("%d %d has died\n", getms(philo), philo->id);
+	}
+	pthread_mutex_unlock(&philo->check_dead);
 }
