df7b1638d13262c331f78f88d0fd1232e123cbcc
fenix144 <yan_1731@hotmail.com>
2022-07-25 09:07:57 -0400

    Fix segmentation fault when there is no optional argument

--- philo/check_utils.c
+++ philo/check_utils.c
@@ -68,10 +68,16 @@ void	check(int argc, char *argv[], t_args *args)
 int	checkintlimit(char *argv[])
 {
 	if (ft_atoi(argv[1]) > INT_MAX || ft_atoi(argv[2]) > INT_MAX
-		|| ft_atoi(argv[3]) > INT_MAX || ft_atoi(argv[4]) > INT_MAX || ft_atoi(argv[5]) > INT_MAX)
+		|| ft_atoi(argv[3]) > INT_MAX || ft_atoi(argv[4]) > INT_MAX)
 	{
 		dprintf(STDERR_FILENO, "Argument needs to be lower than %d\n", INT_MAX);
 		return (1);
 	}
+	if (argv[5])
+		if (ft_atoi(argv[5]) > INT_MAX)
+		{
+			dprintf(STDERR_FILENO, "Argument needs to be lower than %d\n", INT_MAX);
+			return (1);
+		}
 	return (0);
 }
